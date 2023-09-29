663634bbf10d3bbcea3449de8998343dd8f1410e
Asger Thyregod <ath@cvation.com>
2023-09-29 13:51:04 +0200

    Fixed repository creation

--- .github/scripts/CreateRepository.ps1
+++ .github/scripts/CreateRepository.ps1
@@ -10,13 +10,13 @@ $headers = @{
     Accept = 'application/vnd.github.v3+json';
     Authorization = "token $authToken";
     'Content-Type' = 'application/json';
-}
+} 
 
 $postParams = @{
     name=$repositoryName;
     private=$false;
-}
+} | ConvertTo-Json
 
 $url = "https://api.github.com/user/repos" # This should be updated to /orgs/{org}/repos for orgs
 
-Invoke-WebRequest -Uri $url -Method POST -Body $postParams -Headers $headers -UseBasicParsing
+Invoke-WebRequest -Uri $url -Method POST -Body $postParams -Headers $headers
