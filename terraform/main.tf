c6ba2f7db49aad7171061860c41bf7ae7e0586b5
Asger Thyregod <asger.thyregod@gmail.com>
2024-12-30 11:46:20 +0100

    Trying to only add role for product environments which needs it

--- terraform/main.tf
+++ terraform/main.tf
@@ -268,7 +268,7 @@ resource "azurerm_role_assignment" "acr_pull_role_assignment" {
 
 resource "azurerm_role_assignment" "acr_push_role_assignment" {
   for_each = {
-    for product_environment in local.product_environments : product_environment.product_environment => product_environment
+    for product_environment in local.product_environments : product_environment.product_environment => product_environment if product_environment.ContainerRegistryNeeded
   }
 
   scope                = azurerm_container_registry.container_registry[each.value.environment_name].id
