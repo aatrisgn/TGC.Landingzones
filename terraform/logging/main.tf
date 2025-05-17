025a50d1a0dea3230f266a140711366f3793eb04
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2025-05-17 13:52:18 +0200

    Fixed format

--- terraform/logging/main.tf
+++ terraform/logging/main.tf
@@ -28,13 +28,13 @@ resource "azurerm_resource_group" "state_file_resource_group" {
 }
 
 resource "azurerm_log_analytics_workspace" "shared_log_analytic_workspace" {
-    name = "law-logging-shared-${var.environment}-westeurope"
-    resource_group_name = azurerm_resource_group.state_file_resource_group.name
-    location = azurerm_resource_group.state_file_resource_group.location
-    
-    retention_in_days = 30 //Just keeping it the lowest to minimize cost
-    
-    tags = {
+  name                = "law-logging-shared-${var.environment}-westeurope"
+  resource_group_name = azurerm_resource_group.state_file_resource_group.name
+  location            = azurerm_resource_group.state_file_resource_group.location
+
+  retention_in_days = 30 //Just keeping it the lowest to minimize cost
+
+  tags = {
     "provision" = "landingzones"
   }
 }
