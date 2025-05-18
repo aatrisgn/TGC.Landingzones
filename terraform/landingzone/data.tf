35fdafd2a75d90370320962179cf3c1308214bea
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2025-05-18 18:42:34 +0200

    Fixed format

--- terraform/landingzone/data.tf
+++ terraform/landingzone/data.tf
@@ -5,6 +5,6 @@ data "azuread_domains" "example" {
 data "azurerm_client_config" "azurerm_client_config" {}
 
 data "azurerm_log_analytics_workspace" "shared_log_analytic_workspace" {
-  name = var.shared_log_analytic_workspace.name
+  name                = var.shared_log_analytic_workspace.name
   resource_group_name = var.shared_log_analytic_workspace.resource_group_name
 }
