ceedc155ddc6ccfb14300c664aef1389470f9197
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2025-07-13 13:23:05 +0200

    Added provider to data

--- terraform/landingzone/data.tf
+++ terraform/landingzone/data.tf
@@ -5,11 +5,15 @@ data "azuread_domains" "example" {
 data "azurerm_client_config" "azurerm_client_config" {}
 
 data "azurerm_log_analytics_workspace" "shared_dev_log_analytic_workspace" {
+  provider = azurerm.new
+
   name                = var.shared_dev_log_analytic_workspace.name
   resource_group_name = var.shared_dev_log_analytic_workspace.resource_group_name
 }
 
 data "azurerm_log_analytics_workspace" "shared_prd_log_analytic_workspace" {
+  provider = azurerm.new
+
   name                = var.shared_prd_log_analytic_workspace.name
   resource_group_name = var.shared_prd_log_analytic_workspace.resource_group_name
 }
